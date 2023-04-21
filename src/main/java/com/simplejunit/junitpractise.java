a188cafaf26ebf4369b9ccbaef6fd86381c20dab
Nalli-vyshu <nallivyshnavi7@gmail.com>
2023-04-21 08:46:17 +0530

    second day

---
+++ src/main/java/com/simplejunit/junitpractise.java
@@ -0,0 +1,17 @@
+package com.simplejunit;
+
+public class junitpractise {
+	
+	public int biggest (int a,int b) {
+		
+		int c=a>b?a:b;
+		return c;
+	}
+ public String Whatisyourname() {
+	 return "vyshnavi";
+ }
+ 
+ public boolean even(int a) {
+	 return a%2==0;
+ }
+}
---
+++ src/test/java/com/simplejunit/junittestpractise.java
@@ -0,0 +1,56 @@
+package com.simplejunit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+
+public class junittestpractise {
+	
+ junitpractise j1=new junitpractise ();
+ 
+
+	@Test
+	 @DisplayName("vyshnavi")
+	public void testbiggest() {
+		int exp=6;
+		int res=j1.biggest(5,  6);
+		assertEquals(exp,res);
+		
+	}
+ @Test
+ public void testbiggest2() {
+	 
+	 assertNotEquals(3,j1.biggest(3,  4));
+ }
+ @Test
+ public void checkname() {
+	 
+	assertSame("vyshnavi",j1.Whatisyourname());
+ }
+ 
+ @Test
+    public void checkname2() {
+	 
+	 assertNotEquals("java",j1.Whatisyourname());
+	 
+	 
+ }
+ @Test
+  public void eventest() {
+	 boolean res=j1.even(6);
+	 assertTrue(res);
+ }
+ @Test
+ public void testodd() {
+	 boolean res=j1.even(7);
+	 assertFalse(res);
+    
+	 
+ }
+}
